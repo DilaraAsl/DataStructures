12c331131ec67ca9335c7ea1100e1d6f73092837
DilaraAsl <dcavus@gmail.com>
2023-02-22 14:18:17 -0800

    adding new solutions

--- ds_00_groupTasks/src/main/java/com/cydeo/interviewPrep/aExpert/ValidateSubsequence.java
+++ ds_00_groupTasks/src/main/java/com/cydeo/interviewPrep/aExpert/ValidateSubsequence.java
@@ -1,47 +1,54 @@
 package com.cydeo.interviewPrep.aExpert;
+
 import java.util.*;
+
 public class ValidateSubsequence {
     public static void main(String[] args) {
-        List<Integer> array=new ArrayList<>(Arrays.asList(1,5,2,-1,8,6));
-        List<Integer> sequence=new ArrayList<>(Arrays.asList(1,-1,5));
+        List<Integer> array = new ArrayList<>(Arrays.asList(1, 5, 2, -1, 8, 6));
+        List<Integer> sequence = new ArrayList<>(Arrays.asList(1, -1, 5));
 
-        System.out.println(isValidSubsequence2( array, sequence));
+        System.out.println(isValidSubsequence2(array, sequence));
     }
 
     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
         // Write your code here.
 
-        if(sequence.size()==1 && array.contains(sequence.get(0))) return true;
-        List<Integer> list=new ArrayList<>();
-        for (int i = 0; i <sequence.size() ; i++) {
-            for (int j = 0; j <array.size() ; j++) {
-                if(sequence.get(i)==array.get(j)){
-                    list.add(j); break;
+        if (sequence.size() == 1 && array.contains(sequence.get(0))) return true;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < sequence.size(); i++) {
+            for (int j = 0; j < array.size(); j++) {
+                if (sequence.get(i) == array.get(j)) {
+                    list.add(j);
+                    break;
                 }
 
             }
 
         }
 
-        List<Integer> unSortedList=new ArrayList(list);
+        List<Integer> unSortedList = new ArrayList(list);
 
         Collections.sort(list);
 
-        if(unSortedList.equals(list)) return true;
+        if (unSortedList.equals(list)) return true;
         return false;
     }
+
     public static boolean isValidSubsequence2(List<Integer> array, List<Integer> sequence) {
         // Write your code here.
-        boolean[] arr=new boolean[sequence.size()];
-        int pointerA=0;
-        int pointerS=0;
+       boolean[] arr=new boolean[sequence.size()];
+        int pointerA = 0;
+        int pointerS = 0;
 
-        while(pointerS<sequence.size()&&pointerA<array.size()){
-            if(array.get(pointerA)==sequence.get(pointerS)) {arr[pointerS]=true; pointerS++;pointerA++;}
-            else pointerA++;
+        while (pointerS < sequence.size() && pointerA < array.size()) {
+            if (array.get(pointerA) == sequence.get(pointerS)) {
+              arr[pointerS] = true;
+                pointerS++;
+                pointerA++;
+            } else pointerA++;
         }
-     if(arr[sequence.size()-1]==false) return false;
-
-        return true;
+    if(arr[sequence.size()-1]==false) return false;
+//        if (pointerA==array.size()-1&& array.get(pointerA-1) == sequence.get(pointerS)) return true;
+        return false;
     }
 }
