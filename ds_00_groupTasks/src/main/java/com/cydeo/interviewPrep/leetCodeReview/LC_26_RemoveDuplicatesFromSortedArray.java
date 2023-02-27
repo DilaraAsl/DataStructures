c013c0dee5fc74bd576a304e819b4e357e8c0fc5
DilaraAsl <dcavus@gmail.com>
2023-02-27 13:29:44 -0800

    adding new solutions

--- ds_00_groupTasks/src/main/java/com/cydeo/interviewPrep/leetCodeReview/LC_26_RemoveDuplicatesFromSortedArray.java
+++ ds_00_groupTasks/src/main/java/com/cydeo/interviewPrep/leetCodeReview/LC_26_RemoveDuplicatesFromSortedArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LC_26_RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
-        int[] nums={0,0,1,1,1,2,2,3,3,4};
+        int[] nums={1,1,2};
         System.out.println(removeDuplicates(nums));
     }
     public static int removeDuplicates(int[] nums){
